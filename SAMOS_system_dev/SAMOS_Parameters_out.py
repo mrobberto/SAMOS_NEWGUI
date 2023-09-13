759363188e64b7e0c03ad8551999e45c67e7a02a
SAMOS team <robberto@stsci.edu>
2023-09-13 13:01:52 -0400

    Created V3p6
    1) SAMOS_Parameters is now a class in the file SAMOS_Parameters_out.py in the directory SAMOS_system_dev,
    i.e. it has been moved outside from the main file.
    As a class residing in a different file, It is imported now with:
    
    from SAMOS_system_dev.SAMOS_Parameters_out import SAMOS_Parameters
    
    apart from this, it is called in the usual way, i.e.
    
    self.PAR = SAMOS_Parameters()
    
    2) Added in the FITS manager field of the main window a new "Find stars" button that searches for stars down to a pretty low threshold; the maximum number of stars found is still given by the Nr. of stars field used by twirl to get the WCS solution, but one can increase setting e.g. 200 stars. The results this time are regions set as "rectangles", i.e. slits, with the width and height set in the two windows to the right. One can then push "Show Traces", it takes a bit longer but all the ~200 sources show their superimposed traces. At this point the Dana button "generate Patterns" should work, but needs to be fixed.

---
+++ SAMOS_system_dev/SAMOS_Parameters_out.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 17:52:35 2023
+
+@author: samos_dev
+"""
+import tkinter as tk
+import os
+from pathlib import Path
+import sys
+import csv
+
+path = Path(__file__).parent.absolute()
+local_dir = str(path.absolute())
+parent_dir = str(path.parent)
+sys.path.append(parent_dir)
+
+dir_SYSTEM = os.path.join(local_dir, "SAMOS_system_dev")
+
+class SAMOS_Parameters():
+    """ Collection of paramaeters to be shared by the classes """
+
+    def __init__(self):
+        """
+        Defines:
+            self.Image_on/off
+            self.dir_dict
+            self.IP_dict
+            self.IP_status_dict
+            self.inoutvar
+            self.scale_DMD2PIXEL = 0.892
+        """
+
+        self.Image_on = tk.PhotoImage(file=os.path.join(
+            parent_dir, "tk_utilities", "Images", "on.png"))
+        self.Image_off = tk.PhotoImage(file=os.path.join(
+            parent_dir, "tk_utilities", "Images", "off.png"))
+
+        self.dir_dict = {'dir_Motors': '/SAMOS_MOTORS_dev',
+                         'dir_CCD': '/SAMOS_CCD_dev',
+                         'dir_DMD': '/SAMOS_DMD_dev',
+                         'dir_SOAR': '/SAMOS_SOAR_dev',
+                         'dir_SAMI': '/SAMOS_SAMI_dev',
+                         'dir_Astrom': '/SAMOS_Astrometry_dev',
+                         'dir_system': '/SAMOS_system_dev',
+                         }
+
+        """ Default IP address imported for all forms"""
+        ip_file_default = os.path.join(local_dir, "IP_addresses_default.csv")
+        with open(ip_file_default, mode='r') as inp:
+            reader = csv.reader(inp)
+            dict_from_csv = {rows[0]: rows[1] for rows in reader}
+        inp.close()
+        self.IP_dict = {}
+        self.IP_dict['IP_Motors'] = dict_from_csv['IP_Motors']
+        self.IP_dict['IP_CCD'] = dict_from_csv['IP_CCD']
+        self.IP_dict['IP_DMD'] = dict_from_csv['IP_DMD']
+        self.IP_dict['IP_SOAR'] = dict_from_csv['IP_SOAR']
+        self.IP_dict['IP_SAMI'] = dict_from_csv['IP_SAMI']
+
+        self.IP_status_dict = {'IP_Motors': False,
+                               'IP_CCD': False,
+                               'IP_DMD': False,
+                               'IP_SOAR': False,
+                               'IP_SAMI': False,
+                               }
+        """ REMOVED
+        self.IP_dict =  {'IP_Motors': '128.220.146.254:8889',
+                         'IP_CCD'   : '128.220.146.254:8900',
+                         'IP_DMD'   : '128.220.146.254:8888',
+                         'IP_SOAR'  : 'TBD',
+                         'IP_SAMI'  : 'TBD',
+                        } 
+        """
+
+        self.inoutvar = tk.StringVar()
+        self.inoutvar.set("outside")
+
+        self.scale_DMD2PIXEL = 0.892  # mirros to pixel as per e-mail by RB  Jan 207, 2023
+
+        self.dmd_map_filename = ""
