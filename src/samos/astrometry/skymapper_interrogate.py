9735e31ec001ae00a2f04b693d03f6a3065da7f4
york@stsci.edu <york@stsci.edu>
2024-02-23 18:48:53 -0500

    More updates to make the file work a bit better

--- src/samos/astrometry/skymapper_interrogate.py
+++ src/samos/astrometry/skymapper_interrogate.py
@@ -7,6 +7,7 @@ Created on Sun Feb 19 22:05:04 2023
 """
 from astropy import units as u
 from astropy.io import fits
+import logging
 import numpy as np
 import pandas as pd
 import re
@@ -17,21 +18,21 @@ import urllib.request
 from samos.utilities import get_temporary_dir
 
 
-def skymapper_interrogate(ra=189.99763, dec=-11.62305, ra_size=1058, dec_size=1032, filter='r'):
+def skymapper_interrogate(ra, dec, ra_size=1058, dec_size=1032, filter='r'):
     """
     ***** This documentation needs to be checked for accuracy
     Query the skymapper.nci.org.au website for information on the supplied location.
     
     Parameters
     ----------
-    ra : float, default 189.99763
+    ra : float
         RA in decimal degrees of centre of location
-    dec : float, default -11.62305
+    dec : float
         DEC in decimal degrees of centre of location 
-    ra_size : int
-        Size of field in the RA direction (arcseconds)
-    dec_size : int
-        Size of field in the DEC direction (arcseconds)
+    ra_size : int, default=1058
+        Size of field in the RA direction (arcseconds). Default is magic.
+    dec_size : int, default=1032
+        Size of field in the DEC direction (arcseconds). Default is magic.
     filter: str, default 'r'
         Filter to retrieve
     
@@ -41,43 +42,56 @@ def skymapper_interrogate(ra=189.99763, dec=-11.62305, ra_size=1058, dec_size=10
     pixel_scale = 0.18 * u.arcsec / u.pix
     base_url = "https://api.skymapper.nci.org.au/public/siap/dr2/"
     out_dir = get_temporary_dir()
+    
+    logger = logging.getLogger('samos')
 
+    # Handle formatting of values to proper units
     ra = ra * u.deg
     dec = dec * u.deg
     size_ra = (ra_size * u.pixel * pixel_scale).to(u.deg)
     size_dec = (dec_size * u.pixel * pixel_scale).to(u.deg)
     
-    query_url = base_url + "query?POS={},{}&SIZE={},{}&BAND={}FORMAT=image/fits&INTERSECT=covers"
-    query = query_url.format(ra.value, dec.value, size_ra.value, size_dec.value)
+    # Convert units to scalar values for use in the query
+    ra = ra.value
+    dec = dec.value
+    size_ra = size_ra.value
+    size_dec = size_dec.value
     
-    with urllib.request.urlopen(query, timeout=30) as response:
+    query = base_url + "query?POS={},{}&SIZE={},{}&BAND={}&FORMAT=image/fits&INTERSECT=covers"
+    query_url = query.format(ra, dec, size_ra, size_dec, filter)
+    logger.debug("Query URL is {}".format(query))
+    
+    with urllib.request.urlopen(query_url, timeout=30) as response:
        html = response.read()
     
     v=html.decode('utf-8')
     
     entrypoint = []
-    [entrypoint.append(m.start()) for m in re.finditer(">SkyMapper_", v)] 
+    [entrypoint.append(m.start()) for m in re.finditer(">SkyMapper_", v)]
     
+    out_files = []
     for i in range(len(entrypoint)):
         image_number = v[entrypoint[i]+13:entrypoint[i]+30]
         
         image_url = base_url + "get_image?IMAGE={}&SIZE={},{}&POS={},{}&BAND={}&FORMAT=fits"
-        image_query = image_url.format(image_number, ra.value, dec.value, size_ra.value, size_dec.value, filter)
+        image_query = image_url.format(image_number, size_ra, size_dec, ra, dec, filter)
+        logger.info("Retrieving image at {}".format(image_query))
         
         out_file = out_dir / f"{image_number}.fits"
         
         # Fetching URLs
         # FROM https://docs.python.org/3/howto/urllib2.html
         with urllib.request.urlopen(image_query, timeout=30) as response:
-            shutil.copyfileobj(response, out_file)
-        
-        with open(out_file) as html:
-            pass
+            with open(out_file, 'wb') as of:
+                shutil.copyfileobj(response, of)
         
         with fits.open(out_file) as hdu_in:
             header = hdu_in[0].header
-
-        if ((ra_size == 1058) and (dec_size == 1032)):
-            return(out_file)
-        elif np.absolute(header['NAXIS1'] - header['NAXIS2']) <= 1:
-            return(out_file)
+        
+        if np.absolute(header['NAXIS1'] - header['NAXIS2']) > 1.:
+            if  (ra_size != 1058) or (dec_size != 1032):
+                logger.error("Bad Image Size")
+                continue
+        
+        out_files.append(out_file)
+    return out_files
